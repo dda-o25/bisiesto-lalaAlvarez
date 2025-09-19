e38249998f6fe6ad4ddb8640c20432a3e5e75391
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-19 17:24:16 -0600

    primer commit

--- bisiesto.py
+++ bisiesto.py
@@ -1,15 +1,15 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Larisa Carolina Alvarez Gonzalez 763374
 """
 
-# Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
+year = int(input("Introduzca un año: "))
 
 # Proceso
-
-
+if (year % 4  == 0 and year % 100 != 0) or (year % 400 == 0):
+    salida = str(year) + " sí es un año bisiesto"
+else:
+    salida = str(year) + " no es un año bisiesto" 
 # Salidas
 print(salida)
